8eb1596d6eb46acb9dc62e248b8a6a2de30faf6d
umenyioraj <umenyiorajosh@gmail.com>
2026-04-20 00:02:18 -0400

    feat: Generate environment.py from environment variables

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,17 @@
 # exit on error
 set -o errexit
 
+# Generate enviornment.py from env vars (file is gitignored)
+mkdir -p enviornment
+cat > enviornment/enviornment.py <<EOF
+import os
+RESEND_API_KEY = os.environ.get('RESEND_API_KEY', '')
+NEON_URL = os.environ.get('NEON_URL', '')
+NEON_USERNAME = os.environ.get('NEON_USERNAME', '')
+NEON_PASSWORD = os.environ.get('NEON_PASSWORD', '')
+HF_TOKEN = os.environ.get('HF_TOKEN', '')
+EOF
+
 # Upgrade pip
 pip install --upgrade pip
 
